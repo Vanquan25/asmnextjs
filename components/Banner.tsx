e7dac33094d9d2ef02d6ff47a6590820d13d39d8
minh <hkminh1306@gmail.com>
2022-08-01 10:21:46 +0700

    ft: create Homepage

---
+++ components/Banner.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+type Props = {}
+
+const Banner = (props: Props) => {
+    return (
+        <div>
+            <img src="http://meetandmore.vn/wp-content/uploads/2018/02/banner3.jpg" alt="" className='img-fluid'/>
+        </div>
+    )
+}
+
+export default Banner
